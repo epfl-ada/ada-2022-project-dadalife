54d2f80f37e19eae9be0167382a47a029f45feae
rpugin <igotTh3power>
2022-12-22 18:34:22 +0100

    nicer labels for select menus

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,23 @@
 
 const periods = ['1982_1986', '1987_1991', '1992_1996', '1997_2001', '2002_2006', '2007_2011']
-const genres = [
-    "all",
-    "Doomsday_film_Apocalyptic_and_postapocalyptic_fiction_Dystopia_Disaster",
-    "Detective_Detective_fiction_Crime_Thriller_Mystery",
-    "Grossout_film_Gross_out_Romance_Film_Comedy",
-    "Horror_Drama_Slasher_Psychological_thriller",
-    "Family_Film_ChildrensFamily_Comedy_Animation",
-    "Documentary_Culture__Society_Gay_Gay_Interest",
-    "Gay_Gay_Interest_Gay_Themed_LGBT",
-    "Antiwar_Antiwar_film_Environmental_Science_Nature",
-    "Environmental_Science_Nature_Sword_and_sorcery_films_Sword_and_sorcery",
-    "Business_Computers_Finance__Investing_Romance_Film",
-    "Avantgarde_Experimental_film_Surrealism_Essay_Film",
-    "Horror_Bmovie_Cult_Slasher",
-    "Business_Computers_Finance__Investing_Experimental_film",
-    "Documentary_Culture__Society_Biography_Social_issues",
-    "Environmental_Science_Nature_Gross_out_Grossout_film",
-]
+const genres = {
+    "all": "All",
+    "Doomsday_film_Apocalyptic_and_postapocalyptic_fiction_Dystopia_Disaster": "Doomsday film, Apocalyptic and postapocalyptic fiction, Dystopia, Disaster",
+    "Detective_Detective_fiction_Crime_Thriller_Mystery": "Detective, Detective fiction, Crime, Thriller_Mystery",
+    "Grossout_film_Gross_out_Romance_Film_Comedy": "Grossout film, Gross out Romance, Film, Comedy",
+    "Horror_Drama_Slasher_Psychological_thriller": "Horror, Drama, Slasher, Psychological, thriller",
+    "Family_Film_ChildrensFamily_Comedy_Animation": "Family film, Childrens, Family, Comedy, Animation",
+    "Documentary_Culture__Society_Gay_Gay_Interest": "Documentary, Culture, Society, Gay, Gay interest",
+    "Gay_Gay_Interest_Gay_Themed_LGBT": "Gay, Gay interest, Gay themed, LGBT",
+    "Antiwar_Antiwar_film_Environmental_Science_Nature": "Antiwar, Antiwar film, Environmental, Science, Nature",
+    "Environmental_Science_Nature_Sword_and_sorcery_films_Sword_and_sorcery": "Environmental, Science, Nature, Sword and sorcery films, Sword and sorcery",
+    "Business_Computers_Finance__Investing_Romance_Film": "Business, Computers, Finance/Investing, Romance film",
+    "Avantgarde_Experimental_film_Surrealism_Essay_Film": "Avantgarde, Experimental film, Surrealism, Essay film",
+    "Horror_Bmovie_Cult_Slasher": "Horror, Bmovie, Cult, Slasher",
+    "Business_Computers_Finance__Investing_Experimental_film" :"Business, Computers, Finance/Investing, Experimental film",
+    "Documentary_Culture__Society_Biography_Social_issues": "Documentary, Culture, Society, Biography, Social issues",
+    "Environmental_Science_Nature_Gross_out_Grossout_film": "Environmental, Science, Nature, Gross out, Grossout film",
+}
 
 function update_current_hist(){
 
@@ -43,9 +43,9 @@ function init_select() {
         document.getElementById("s_year").insertAdjacentHTML("beforeend", elem)
     }
 
-    for(let i=0; i<genres.length;i++){
-        let g = genres[i]
-        let display_genre  = g.replaceAll("_", ", ")
+    for(const [key, val] of Object.entries(genres)){
+        let g = key
+        let display_genre  = val
         let elem = "<option value='"+g+"'>"+display_genre + "</option>"
         document.getElementById("s_genre").insertAdjacentHTML("beforeend", elem)
     }
